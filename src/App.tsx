32b3c5938801298dbdf5d485fbf0fc2581d043aa
jharry444 <jharrison1@qa.com>
2025-02-11 12:39:01 +0000

    added explanations

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import About from "./components/About"
+// import About from "./components/About"
 import Trainer from "./components/Trainer";
 
 function App() {
 
   const intro = () => console.log("hello, world!");
-  
-  return (    
+
+  return (
     <>
-        <h1>Hello, World</h1>
-        {/* <About/> */}
-        <Trainer name="Jordan" age={30} specialism="java" intro={intro}/>
-        <Trainer name="Cameron" age="32" specialism="js"  intro={intro}/>
-        <Trainer name="Piers" age={26} specialism="devops"  intro={intro} />
-        <Trainer name="Shafeeq" age={40} specialism="python"  intro={intro}/>
-        <Trainer name="Peter" />
-        <input />
+      <h1>Hello, World</h1>
+      {/* <About/> */}
+      {
+        /* when passing a function as a prop remmber to NOT use () as you want to 
+        pass a REFERENCE to the function, rather than the RESULT of calling it  */
+      }
+      <Trainer name="Jordan" age={30} specialism="java" intro={intro} />
+      <Trainer name="Cameron" age="32" specialism="js" intro={intro} />
+      <Trainer name="Piers" age={26} specialism="devops" intro={intro} />
+      <Trainer name="Shafeeq" age={40} specialism="python" intro={intro} />
+      <Trainer name="Peter" />
+      {/* In react self-closing elements must have a closing / */}
+      <input />
     </>
   )
 }
--- src/components/Trainer.tsx
+++ src/components/Trainer.tsx
@@ -1,9 +1,14 @@
+// define a type in the same file as it's not used anywhere else atm
+// ? denotes an optional paremeter
+// | shows that a parameter can be one of 2 (or more) types
+// easy method definition: name: (param: type) => returnType
 type TrainerProps = { 
     name?: string , 
     age?: number | string, 
     specialism?: string
     intro?: () => void
 }
+// for convenience the props have been 'destructured' (or 'exploded')
 
 const Trainer = ({name, age, specialism, intro}: TrainerProps) => {
 
