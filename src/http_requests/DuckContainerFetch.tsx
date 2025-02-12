0bba02f1d54d7f82417c8e38b847ad6647a749b1
jharry444 <jharrison1@qa.com>
2025-02-12 16:43:16 +0000

    useMemo example

--- src/http_requests/DuckContainerFetch.tsx
+++ src/http_requests/DuckContainerFetch.tsx
@@ -1,10 +1,11 @@
 import DuckForm from "./DuckForm";
 import DuckList from "./DuckList";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Duck } from "./types";
 import axios from "axios";
 
 const DuckContainerFetch = () => {
+    const [contrived, setContrived] = useState("");
     const [filter, setFilter] = useState("");
     const [ducks, setDucks] = useState<Duck[]>([]);
 
@@ -23,11 +24,20 @@ const DuckContainerFetch = () => {
     }, [filter]);
 
 
+    const filteredDucks = useMemo<Duck[]>(() => {
+        return ducks.filter(duck => {
+            console.log("I'M FILTERING DUCKS!");
+            return duck.name.toLowerCase().startsWith(filter);
+        })
+    }, [ducks, filter])
+
     return (<>
         <DuckForm fetchDucks={fetchDucks} />
+        <h3>CONTRIVED INPUT</h3>
+        <input type="text" value={contrived} onChange={e => setContrived(e.target.value)} />
         <h3>Filter</h3>
         <input type="text" placeholder="name" value={filter} onChange={e => setFilter(e.target.value)} />
-        <DuckList ducks={ducks.filter(duck => duck.name.toLowerCase().startsWith(filter))} />
+        <DuckList ducks={filteredDucks} />
     </>);
 }
 
